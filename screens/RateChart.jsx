2eefb3750ad7a68a9faaeeed42232608ccf64036
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-06-29 21:32:03 +0530

    Farmer and Rate Chart

--- screens/RateChart.jsx
+++ screens/RateChart.jsx
@@ -1,47 +1,111 @@
-import {
-  View,
-  Text,
-  StyleSheet
-} from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { Button } from 'react-native-paper'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Button, IconButton, MD3Colors } from 'react-native-paper';
+import { FlatList } from 'react-native';
+import AddRateDetails from './AddRateDetails';
 
 const RateChart = () => {
+  const navigator = useNavigation();
+  const [search, setSearch] = useState('');
 
   const data = [
     {
       category: 'KGFAT + KGSNF',
       name: 'Standard RC',
       ratio: '60:40',
-      level: 'Level 1',
-      type: 'Cow',
-    }
-  ]
+      level: 1,
+      type: 'COW',
+    },
+    {
+      category: 'KGFAT + KGSNF',
+      name: 'Standard RC',
+      ratio: '60:40',
+      level: 1,
+      type: 'BUFFALO',
+    },
+  ];
 
-  const navigator = useNavigation()
-
-  useLayoutEffect(() => {
-    navigator.setOptions({ headerShown: false });
-  }, [navigator])
+  const Item = ({ category, name, level, type }) => (
+    <View style={styles.item}>
+      <View style={styles.itemDetails}>
+        <View style={styles.wrapper}>
+          <Text style={{ fontWeight: '500', fontSize: 16 }}>{name}</Text>
+          <Text style={{ color: 'red' }}>({type})</Text>
+        </View>
+        <View style={styles.wrapper}>
+          <Text>Level {level}</Text>
+          <Text>({category})</Text>
+        </View>
+      </View>
+      <IconButton
+        icon='pencil'
+        iconColor={MD3Colors.error50}
+        size={20}
+        onPress={() => console.log('Rate chart edited')}
+      />
+      <IconButton
+        icon='delete'
+        iconColor={MD3Colors.error50}
+        size={20}
+        onPress={() => console.log('Rate chart deleted')}
+      />
+    </View>
+  );
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data.filter(({ name }) => name.toLowerCase().includes(search.toLowerCase()))}
+        renderItem={({ item }) => (
+          <Item
+            name={item.name}
+            level={item.level}
+            type={item.type}
+            category={item.category}
+            key={item.name}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+      />
 
-
-      <Button mode='contained'>
+      <Button
+        icon='plus'
+        mode='contained'
+        onPress={() => navigator.navigate(AddRateDetails)}
+        style={styles.button}
+      >
         Add Rate Chart
       </Button>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginBottom: 2,
+  },
+  item: {
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    flexDirection: 'row',
+    padding: 10,
+  },
+  itemDetails: {
+    width: 300,
+  },
+  wrapper: {
+    flexDirection: 'row',
+    padding: 5,
+    alignItems: 'center',
+  },
+  button: {
+    padding: 4,
+  },
+});
 
-export default RateChart
+export default RateChart;
